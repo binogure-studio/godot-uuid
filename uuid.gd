6dd8b9a0d1f4db79a9ce1bba0481a275ff9eaf1e
Xavier Sellier <xsellier@users.noreply.github.com>
2020-10-13 15:11:00 +0200

    Update README.md
    Improve performance by inlining methods and avoiding loops
    
    This provides a 6× performance improvement in debug builds.
    
    Note that the plugin will no longer call `randomize()` internally.
    This means the user must call `randomize()` in their project on startup
    instead.
    
    Revert some changes from Calinou and add unit tests

--- uuid.gd
+++ uuid.gd
@@ -1,32 +1,41 @@
+# Note: The code might not be as pretty it could be, since it's written
+# in a way that maximizes performance. Methods are inlined and loops are avoided.
 extends Node
 
-static func getRandomInt(max_value):
+const MODULO_8_BIT = 256
+
+static func getRandomInt():
+  # Randomize every time to minimize the risk of collisions
   randomize()
 
-  return randi() % max_value
-
-static func randomBytes(n):
-  var r = []
-
-  for index in range(0, n):
-    r.append(getRandomInt(256))
-
-  return r
+  return randi() % MODULO_8_BIT
 
 static func uuidbin():
-  var b = randomBytes(16)
-
-  b[6] = (b[6] & 0x0f) | 0x40
-  b[8] = (b[8] & 0x3f) | 0x80
-  return b
+  # 16 random bytes with the bytes on index 6 and 8 modified
+  return [
+    getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt(),
+    getRandomInt(), getRandomInt(), ((getRandomInt()) & 0x0f) | 0x40, getRandomInt(),
+    ((getRandomInt()) & 0x3f) | 0x80, getRandomInt(), getRandomInt(), getRandomInt(),
+    getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt(),
+  ]
 
 static func v4():
+  # 16 random bytes with the bytes on index 6 and 8 modified
   var b = uuidbin()
 
-  var low = '%02x%02x%02x%02x' % [b[0], b[1], b[2], b[3]]
-  var mid = '%02x%02x' % [b[4], b[5]]
-  var hi = '%02x%02x' % [b[6], b[7]]
-  var clock = '%02x%02x' % [b[8], b[9]]
-  var node = '%02x%02x%02x%02x%02x%02x' % [b[10], b[11], b[12], b[13], b[14], b[15]]
+  return '%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x' % [
+    # low
+    b[0], b[1], b[2], b[3],
 
-  return '%s-%s-%s-%s-%s' % [low, mid, hi, clock, node]
+    # mid
+    b[4], b[5],
+
+    # hi
+    b[6], b[7],
+
+    # clock
+    b[8], b[9],
+
+    # clock
+    b[10], b[11], b[12], b[13], b[14], b[15]
+  ]
